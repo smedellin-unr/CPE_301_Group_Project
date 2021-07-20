425143c0847d10a775e606dc915810c019848304
smedellin-unr <smedellin@nevada.unr.edu>
2021-07-20 12:14:16 -0700

    added if statement to only execute RFID logic when user is not authorized

--- Project/src/main.ino
+++ Project/src/main.ino
@@ -1,13 +1,16 @@
 //group project
+#include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
+#include <dht_nonblocking.h>
 
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+#define DHT_SENSOR_PIN 2
 #define SS_PIN 53
-#define RST_PIN 5
+#define RST_PIN 13
 
 #define A0_PIN 0
-#define MAX_BITS_IN_REGISTER 8
 
 //This is for LCD change info button
 //define port H Register pointers
@@ -30,8 +33,12 @@ volatile unsigned char* pin_e  = (unsigned char*) 0x2C;
 volatile unsigned char* port_g = (unsigned char*) 0x34;
 volatile unsigned char* ddr_g  = (unsigned char*) 0x33;
 volatile unsigned char* pin_g  = (unsigned char*) 0x32;
+// define port A register pointers
+volatile unsigned char* port_a = (unsigned char*) 0x22;
+volatile unsigned char* ddr_a  = (unsigned char*) 0x21;
+volatile unsigned char* pin_a  = (unsigned char*) 0x20;
 
-
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
 MFRC522::MIFARE_Key key;
@@ -42,79 +49,73 @@ LiquidCrystal lcd(7,8,9,10,11,12);
 bool buttonPress();
 bool buttonReleased();
 void adc_init();
-uint16_t adc_read(uint8_t adc_channel);
+uint8_t adc_read(uint8_t adc_channel);
 byte nuidPICC[4];
 volatile uint8_t water_level = 0;
 volatile bool authorized = false;
 volatile int screenstate = 0;
 
 void setup() {
-  //code from TENEKO for RFID
   Serial.begin(9600);
-  SPI.begin(); // Init SPI bus
+  SPI.begin();     // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
-  // initialize ADC for 8 bit resolution
-  adc_init();
+  adc_init();      // initialize ADC for 8 bit resolution
 
   for (byte i = 0; i < 6; i++) {
     key.keyByte[i] = 0xFF;
   }
-  
-  Serial.println(F("This code scan the MIFARE Classsic NUID."));
-  Serial.print(F("Using the following key:"));
-  //printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 
-  //My LCD and Button code
-  //begin statement for LCD requiring the number of columns and rows:
   lcd.begin(16, 2);
   //WATER LEVEL SENSOR
   //*ddr_f &= ~(0x01);
   //BUTTON
-  *ddr_h &= ~(0x08);
-  *port_h |= (0x08);
+  *ddr_a &= ~(1 << PA1);
+  *port_a |= (1 << PA1);
   //LED
   *ddr_b |= 0x80;
 }
 
 void loop() {
-  
-  while(!rfid.PICC_ReadCardSerial()&& !rfid.PICC_IsNewCardPresent()){
-    lcd.setCursor(0, 0);
-    lcd.print(" Not Authorized ");
-    lcd.setCursor(0, 1);
-    lcd.print("                ");
-    while (buttonPress()){
-      lcd.setCursor(0, 0);
-      lcd.print("   PLEASE USE   ");
-      lcd.setCursor(0, 1);
-      lcd.print("   RFID CARD    ");
-      delay(200);
-    }
-  }
-  //read card
-  Serial.print(F("PICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  Serial.println(rfid.PICC_GetTypeName(piccType));
-
-  // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) 
+  if( !authorized) 
   {
-   Serial.println(F("Your tag is not of type MIFARE Classic."));
-   authorized = false;
-  } else {
-    //Activates LCD for use
-    authorized = true;
-    Serial.println(F("Authorized User"));
-  }
-  
-  // Halt PICC
-  rfid.PICC_HaltA();
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+    while(!rfid.PICC_ReadCardSerial()&& !rfid.PICC_IsNewCardPresent()){
+      lcd.setCursor(0, 0);
+      lcd.print(" Not Authorized ");
+      lcd.setCursor(0, 1);
+      lcd.print("                ");
+      while (buttonPress()){
+        lcd.setCursor(0, 0);
+        lcd.print("   PLEASE USE   ");
+        lcd.setCursor(0, 1);
+        lcd.print("   RFID CARD    ");
+        delay(200);
+      }
+    }
+    //read card
+    Serial.print(F("PICC type: "));
+    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+    Serial.println(rfid.PICC_GetTypeName(piccType));
 
-  if (authorized = true){
+    // Check is the PICC of Classic MIFARE type
+    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K) 
+    {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    authorized = false;
+    } else {
+      //Activates LCD for use
+      authorized = true;
+      Serial.println(F("Authorized User"));
+    }
+    
+    // Halt PICC
+    rfid.PICC_HaltA();
+    // Stop encryption on PCD
+    rfid.PCD_StopCrypto1();
+  }
+  if(authorized = true)
+  {
     delay(100);
     for (int i = 0; i < 3; i++){
       lcd.setCursor(0, 0);
@@ -156,12 +157,10 @@ void loop() {
    }
   }
 }
-   
-
 
 
 bool buttonPress(){
-  if (*pin_h & (0x08)){  
+  if (*pin_a & (1 << PA1)){  
     *port_b |= 0x80; //LED HIGH
     return false;
    } else {
@@ -177,15 +176,15 @@ void adc_init() {
   ADCSRA |= (1 << ADEN) | (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
 }
 
-uint16_t adc_read(uint8_t adc_channel) {
+uint8_t adc_read(uint8_t adc_channel) {
   // Clears DIDR0 and DIDR2
   DIDR0 = 0x00;
   DIDR2 = 0x00;
   // Disables appropriate digital input buffer
-  if (adc_channel < MAX_BITS_IN_REGISTER)
+  if (adc_channel < 8)
     DIDR0 |= (1 << adc_channel);
   else 
-    DIDR2 |= (1 << (adc_channel - MAX_BITS_IN_REGISTER));
+    DIDR2 |= (1 << (adc_channel - 8));
   // Start sample
   ADCSRA |= (1 << ADSC);
   // Wait till sampling complete
