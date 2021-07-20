622adc7111046819a2835b8f8b06236c74f46c40
smedellin-unr <smedellin@nevada.unr.edu>
2021-07-20 12:17:35 -0700

    removed serial print instructions as they were getting in the way of execution

--- Project/src/main.ino
+++ Project/src/main.ino
@@ -1,5 +1,4 @@
 //group project
-#include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
@@ -92,21 +91,17 @@ void loop() {
       }
     }
     //read card
-    Serial.print(F("PICC type: "));
     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-    Serial.println(rfid.PICC_GetTypeName(piccType));
 
     // Check is the PICC of Classic MIFARE type
     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
       piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
       piccType != MFRC522::PICC_TYPE_MIFARE_4K) 
     {
-    Serial.println(F("Your tag is not of type MIFARE Classic."));
     authorized = false;
     } else {
       //Activates LCD for use
       authorized = true;
-      Serial.println(F("Authorized User"));
     }
     
     // Halt PICC
